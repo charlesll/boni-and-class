3a1e3f0077aac6a90946b88911a712bf7a7138eb
Charles Le Losq <charles.lelosq@gmail.com>
2019-05-07 16:25:54 +1000

    Add docs to boninite()

--- boninites/boninites.py
+++ boninites/boninites.py
@@ -4,9 +4,33 @@ from sklearn import preprocessing
 import numpy as np
 
 class boninite():
-    
+    """classifies boninite magmas using a dataset of chemical elements
+
+    Attributes
+    ----------
+    X : Pandas dataframe
+        The dataframe containing the chemical analysis
+    elements : List
+        The name of the first and last element to include in the classification
+    imputer : sklearn imputer
+        Impute missing values. Default is the sklearn.impute.SimpleImputer() that replaces missing values with the mean of the column
+    X_imputed : ndarray
+        Imputed X values
+    scaler : sklearn scaler
+        The scaler to use. Default is the sklearn.preprocessing.StandardScaler()
+    X_sc : ndarray
+        Scaled and imputed X values
+    model : sklearn classification model
+
+    Methods
+    -------
+    model.fit(self)
+        Classifies the X_sc dataset created after initialization
+    model.predict(self,data)
+        Predictions on new data (Pandas dataframe)
+    """
     def __init__(self, model, data, elements,file_format="csv"):
-        
+
         self.X = data.loc[:,elements[0]:elements[1]] # for training the clustering algo
         self.elements = elements
         # impute
@@ -16,12 +40,12 @@ class boninite():
         # Standardization
         self.scaler = preprocessing.StandardScaler().fit(self.X_imputed)
         self.X_sc = self.scaler.transform(self.X_imputed)
-    
+
         self.model = model
-        
+
     def fit(self):
         self.model.fit(self.X_sc)
-        
+
     def predict(self,data):
         X_ = data.loc[:,self.elements[0]:self.elements[1]] # for training the clustering algo
         return self.model.predict(self.scaler.transform(self.imputer.transform(X_)))
